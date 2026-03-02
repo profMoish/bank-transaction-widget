9e590ab25ac8a8341dab857c5ff88c48808a84cd
Mr Moısh <Basilio>
2026-03-02 11:24:22 -0300

    Функцию маскировки номера банковской карты и банковского счета

---
+++ src/masks.py
@@ -0,0 +1,8 @@
+def get_mask_card_number(card_number: str) -> str:
+    """Функцию маскировки номера банковской карты"""
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
+
+
+def get_mask_account(account: str) -> str:
+    """"Функцию маскировки номера банковского счета"""
+    return f"**{account[-4:]}"
