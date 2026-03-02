37f0d2fe8c27dcb6296aa1eaa6b535ab04e33ae9
Mr Moısh <Basilio>
2026-03-02 14:50:00 -0300

    Функция форматирования даты

--- src/widget.py
+++ src/widget.py
@@ -7,3 +7,8 @@ def mask_account_card(number: str) -> str:
         return number[:-20] + masks.get_mask_account(number[-20:])
     else:
         return number[:-16] + masks.get_mask_card_number(number[-16:])
+
+
+def get_data(date: str) -> str:
+    """Функция форматирования даты"""
+    return f'{date[8:10]}.{date[5:7]}.{date[0:4]}'
