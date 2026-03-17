a1b29012048d6f11725f61a0b87137c315a83233
mrMoish <@>
2026-03-17 17:02:38 -0300

    hotfix

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,8 @@
-def filter_by_state(_list, state='EXECUTED') -> list:
+def filter_by_state(_list: list, state: str = 'EXECUTED') -> list:
     """Функция филтрует список словарей по ключю state."""
     return [transaction for transaction in _list if transaction['state'] == state]
 
 
-def sort_by_date(_list, reverse=True) -> list:
+def sort_by_date(_list: list, reverse: bool = True) -> list:
     """Функция возвращает новый список, отсортированный по дате"""
     return sorted(_list, key=lambda x: x['date'], reverse=reverse)
