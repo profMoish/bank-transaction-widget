28758844ad8cdc8e25f37e9c3c7950ea59a8047b
mrMoish <@>
2026-03-18 19:57:52 -0300

    покрыл тестами модуль widget

--- src/widget.py
+++ src/widget.py
@@ -1,14 +1,25 @@
-import masks
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def mask_account_card(number: str) -> str:
     """Функцию маскировки номера банковской карты и номера банковского счета"""
-    if "Счет" in number:
-        return number[:-20] + masks.get_mask_account(number[-20:])
+
+    _list = number.split(' ')
+
+
+    if _list[0] == 'Счет' and len(_list) == 2:
+        if _list[1].isdigit():
+            return _list[0] + " " + get_mask_account(_list[1])
+        else:
+            raise ValueError('В номере счета не должно быть букв')
+    elif len(_list[-1]) == 16 and _list[-1].isdigit():
+        return number[:-16] + get_mask_card_number(_list[-1])
     else:
-        return number[:-16] + masks.get_mask_card_number(number[-16:])
+        raise ValueError('Неверный формат')
 
 
 def get_data(date: str) -> str:
     """Функция форматирования даты"""
+    if not date or not date.strip():
+        raise ValueError('строка с датой пуста')
     return f'{date[8:10]}.{date[5:7]}.{date[0:4]}'
---
+++ tests/test_widget.py
@@ -0,0 +1,48 @@
+import pytest
+
+from src.widget import mask_account_card, get_data
+
+@pytest.mark.parametrize('number, expected', [
+    ('Visa Platinum 7000792289606361', 'Visa Platinum 7000 79** **** 6361'),
+    ('Maestro 7000792289606361', 'Maestro 7000 79** **** 6361'),
+    ('Счет 73654108430135874305', 'Счет **4305'),
+    ('Maestro 1596837868705199', 'Maestro 1596 83** **** 5199'),
+    ('Счет 64686473678894779589', 'Счет **9589'),
+    ('MasterCard 7158300734726758', 'MasterCard 7158 30** **** 6758'),
+    ('Счет 35383033474447895560', 'Счет **5560'),
+    ('Visa Classic 6831982476737658', 'Visa Classic 6831 98** **** 7658'),
+    ('Visa Platinum 8990922113665229', 'Visa Platinum 8990 92** **** 5229'),
+    ('Visa Gold 5999414228426353', 'Visa Gold 5999 41** **** 6353'),
+    ('Счет 73654108430135874305', 'Счет **4305')
+])
+def test_mask_account_card(number, expected):
+    assert mask_account_card(number) == expected
+
+@pytest.mark.parametrize('number', [
+    'Maestro 70007922896063600',
+    'Счет73654108430135874305',
+    'Счет 1 73654108430135874305',
+    'Счет 7365410843013587430a',
+    'СчетСчетСчетСчет',
+    'Счет',
+    '',
+])
+def test_error_mask_account_card(number):
+    with pytest.raises(ValueError) as e:
+        mask_account_card(number)
+    print(e.value)
+
+
+
+@pytest.mark.parametrize('date, expected', [
+    ('2024-03-11T02:26:18.671407', '11.03.2024'),
+    ('2024-03-12', '12.03.2024'),
+    ('2024/03/12', '12.03.2024'),
+])
+def test_get_data(date, expected):
+    assert get_data(date) == expected
+
+
+def test_error_get_data():
+    with pytest.raises(ValueError):
+        get_data('')
