ca7749dbd99473539bab883b0c5a4717877e24c4
mrMoish <@>
2026-03-19 13:50:39 -0300

    add info about tests in README.md

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,11 @@
 from src.widget import get_data
 
 
-def filter_by_state(_list: list, state: str = 'EXECUTED') -> list:
+def filter_by_state(_list: list, state: str = "EXECUTED") -> list:
     """Функция филтрует список словарей по ключю state."""
-    return [transaction for transaction in _list if transaction['state'] == state]
+    return [transaction for transaction in _list if transaction["state"] == state]
 
 
 def sort_by_date(_list: list, reverse: bool = True) -> list:
     """Функция возвращает новый список, отсортированный по дате"""
-    return sorted(_list, key=lambda x: get_data(x['date']) , reverse=reverse)
+    return sorted(_list, key=lambda x: get_data(x["date"]), reverse=reverse)
--- src/widget.py
+++ src/widget.py
@@ -6,21 +6,21 @@ from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(number: str) -> str:
     """Функцию маскировки номера банковской карты и номера банковского счета"""
 
-    _list = number.split(' ')
+    _list = number.split(" ")
 
-    if _list[0] == 'Счет' and len(_list) == 2:
+    if _list[0] == "Счет" and len(_list) == 2:
         if _list[1].isdigit():
             return _list[0] + " " + get_mask_account(_list[1])
         else:
-            raise ValueError('В номере счета не должно быть букв')
+            raise ValueError("В номере счета не должно быть букв")
     elif len(_list[-1]) == 16 and _list[-1].isdigit():
         return number[:-16] + get_mask_card_number(_list[-1])
     else:
-        raise ValueError('Неверный формат')
+        raise ValueError("Неверный формат")
 
 
 def get_data(date: str) -> str:
     """Функция форматирования даты"""
     if not date or not date.strip():
-        raise ValueError('строка с датой пуста')
+        raise ValueError("строка с датой пуста")
     return datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
