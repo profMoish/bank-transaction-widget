8085c4bec685cd2ac042adc38b3f632ceacbbf00
mrMoish <@>
2026-03-29 18:28:01 -0300

    hotfix

--- src/csv_excel.py
+++ src/csv_excel.py
@@ -1,19 +1,17 @@
 import pandas as pd
-from pandas import DataFrame
-from pandas.io.parsers import TextFileReader
 
 
 def get_operations_form_csv_file(path: str):
-    """ Returns a pandas DataFrame from a csv file """
+    """Returns a pandas DataFrame from a csv file"""
     try:
-        return pd.read_csv(path)
-    except FileNotFoundError as e:
+        return pd.read_csv(path).to_dict(orient="records")
+    except FileNotFoundError:
         return []
 
 
 def get_operations_form_excel_file(path: str):
-    """ Returns a pandas DataFrame from a excel file """
+    """Returns a pandas DataFrame from a excel file"""
     try:
-        return pd.read_excel(path)
-    except FileNotFoundError as e:
+        return pd.read_excel(path).to_dict(orient="records")
+    except FileNotFoundError:
         return []
--- tests/test_csv_excel.py
+++ tests/test_csv_excel.py
@@ -7,6 +7,7 @@ def test_get_operation_form_csv_file() -> None:
     r = get_operations_form_csv_file("data/transaction.csv")
     assert r == []
 
+
 def test_get_operation_form_excel_file() -> None:
     r = get_operations_form_excel_file("data/transactions_excel.xlsx")
     assert len(r) == 1000
