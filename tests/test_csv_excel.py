3d5f4d3483d335d4552b0bccf1e0b5975911630a
mrMoish <@>
2026-03-30 08:55:54 -0300

    hotfix

--- tests/test_csv_excel.py
+++ tests/test_csv_excel.py
@@ -1,3 +1,5 @@
+from unittest.mock import Mock, patch
+
 from src.csv_excel import get_operations_form_csv_file, get_operations_form_excel_file
 
 
@@ -8,8 +10,14 @@ def test_get_operation_form_csv_file() -> None:
     assert r == []
 
 
+def test_get_operation_form_csv_file_with_patch() -> None:
+    with patch("pandas.read_csv") as mock_read_csv:
+        mock_read_csv.return_value.to_dict.return_value = [{"col1": 1, "col2": 2}]
+        r = get_operations_form_csv_file("data/transactions.csv")
+        assert len(r) == 1
+
 def test_get_operation_form_excel_file() -> None:
     r = get_operations_form_excel_file("data/transactions_excel.xlsx")
     assert len(r) == 1000
-    r = get_operations_form_excel_file("data/transaction.csv")
+    r = get_operations_form_excel_file("data/transaction.xlsx")
     assert r == []
